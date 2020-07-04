c49d185dd25ba1c7d2ef82498f58a2d9563a9ad2
tsenghc <danny2953053@gmail.com>
2020-07-05 02:24:49 +0800

    Youtube API oauth object

---
+++ oauth/api_oauth.py
@@ -0,0 +1,9 @@
+import googleapiclient.discovery
+import os
+from utils.config import API_DEVELOPER_KEY
+
+api_service_name = "youtube"
+api_version = "v3"
+
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=API_DEVELOPER_KEY)
