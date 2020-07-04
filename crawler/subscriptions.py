ae78fa0a336d34a0df6fa82f3847ff0bdfef29b2
tsenghc <danny2953053@gmail.com>
2020-07-05 02:51:37 +0800

    Add get_subscriber_by_id

---
+++ crawler/subscriptions.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from oauth.api_oauth import youtube
+from utils import error_code
+
+
+def get_subscriber_by_id(channelId, maxResult):
+    """Get a subscriber raw data
+
+    Args:
+        channelId ([str]) :Youtube channelId
+        maxResult ([int]) :Acceptable values are 0 to 50, inclusive.
+                            The default value is 5.
+
+    Returns:
+        [dict]:Subscriptions API raw data
+        [int]:API error(400)
+
+    """
+    try:
+        request_subscriber = youtube.subscriptions().list(
+            part="snippet",
+            channelId=channelId,
+            maxResults=maxResult
+        )
+        subscribers = request_subscriber.execute()
+    except Exception as e:
+        print(e)
+
+    if not isinstance(subscribers, dict):
+        return error_code.SUBSCRIPTIONS_API_ERROR
+
+    return subscribers
+
+
+if __name__ == "__main__":
+    print(get_subscriber_by_id(channelId="UCIF_gt4BfsWyM_2GOcKXyEQ", maxResult=50))
