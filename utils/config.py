8aa5effdeb97449fdc46f754875006da01bdfaa8
tsenghc <danny2953053@gmail.com>
2020-07-05 02:48:42 +0800

    Upload config and error_code

---
+++ utils/config.py
@@ -0,0 +1,10 @@
+import os
+
+try:
+    API_DEVELOPER_KEY = os.environ["DEVELOPER_KEY"]
+except KeyError:
+    print(KeyError)
+    # The API_DEVELOPER_KEY is YouTube_Data_API_v3_key
+    API_DEVELOPER_KEY = ""
+
+
