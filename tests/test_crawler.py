ba6f4cea448db4685745989ea66f458083ce78f0
tsenghc <danny2953053@gmail.com>
2020-07-10 01:41:11 +0800

    Update crawer unittest
    新增測試資料 並增加頻道清單測試

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,21 +1,33 @@
 import unittest
 
-from crawler import subscriptions, channels
+from crawler import subscriptions, channels, playlists
 
 
 class crawlers(unittest.TestCase):
     def setUp(self) -> None:
-        self.channel_list = ["UCIF_gt4BfsWyM_2GOcKXyEQ", "UC8TtAsZE51ekqffnNASo7DA", "UCAfAQOlKYd6ECvqEMiMrjaA"]
+        self.channel_list = ["UCIF_gt4BfsWyM_2GOcKXyEQ", "UC8TtAsZE51ekqffnNASo7DA", "UCAfAQOlKYd6ECvqEMiMrjaA",
+                             "UC1ppNp9vab0qlkUWZvnLl0A", "UCnJEWsS5agXCkqIpyHC9Grg", "UCK3Ycl9dcHk0qz8yoN-6phA",
+                             "UC-lHJZR3Gqxm24_Vd_AJ5Yw"]
 
     def test_subscriptions(self):
         for channel in self.channel_list:
             result = subscriptions.foreach_subscriber_by_channel(channel)
+            print(result)
             result = isinstance(result, list) or isinstance(result, int)
             self.assertTrue(result)
 
     def test_channels_detail(self):
         for channel in self.channel_list:
-            result = isinstance(channels.get_channel_detail(channel), dict)
+            result = channels.get_channel_detail(channel)
+            print(result)
+            result = isinstance(result, dict)
+            self.assertTrue(result)
+
+    def test_get_all_playlist(self):
+        for channel in self.channel_list:
+            result = playlists.foreach_playlistId_by_channel(channelId=channel)
+            print(result)
+            result = isinstance(result, list) or isinstance(result, int)
             self.assertTrue(result)
 
 
