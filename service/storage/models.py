58e7a46bba390d77ad78d139552d8e44ba1103ad
tsenghc <danny2953053@gmail.com>
2020-09-04 04:23:31 +0800

    Update data model
    大幅更新資料模型

--- service/storage/models.py
+++ service/storage/models.py
@@ -9,186 +9,180 @@ db.init_app(app)
 
 class Subscriptions(db.Model):
     __tablename__ = 'subscriptions'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    resourceChannelId = db.Column(db.String(24), primary_key=False, nullable=False)
-    originalChannelId = db.Column(db.String(24), nullable=False)
-    subscriptAt = db.Column(db.DateTime, nullable=False)
-    resourceTitle = db.Column(db.String(100), nullable=False)
-    resourceDescription = db.Column(db.String(1000), nullable=True)
-    writeTime = db.Column(db.DateTime, default=func.now(), nullable=False)
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    resource_channel_id = db.Column(db.String(24), index=True, unique=True, nullable=False)
+    original_channel_id = db.Column(db.String(24),
+                                    ForeignKey("channel_list.channel_id"),
+                                    nullable=False)
+    subscript_at = db.Column(db.DateTime, nullable=False)
+    update_time = db.Column(db.DateTime, default=func.now(), nullable=False)
 
-    def __init__(self,
-                 resourceChannelId, originalChannelId,
-                 subscriptAt, resourceTitle,
-                 resourceDescription, writeTime):
-        self.resourceChannelId = resourceChannelId
-        self.originalChannelId = originalChannelId
-        self.subscriptAt = subscriptAt
-        self.resourceTitle = resourceTitle
-        self.resourceDescription = resourceDescription
-        self.writeTime = writeTime
+    def __init__(self, **kwargs):
+        super(Subscriptions, self).__init__(**kwargs)
 
 
-class ChannelDetail(db.Model):
-    __tablename__ = 'channel'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    channelId = db.Column(db.String(24), nullable=False)
-    channelTitle = db.Column(db.String(100), nullable=False)
-    channelDescription = db.Column(db.String(100), nullable=False)
-    channelCustomUrl = db.Column(db.String(1000), nullable=False)
-    channelPublishedAt = db.Column(db.DateTime, nullable=False)
-    channelThumbnailsUrl = db.Column(db.String(200), nullable=False)
-    channelCountry = db.Column(db.String(2), nullable=False)
-    channelRelatedPlaylists = db.Column(db.String(24), nullable=False)
-    channelViewCount = db.Column(db.Integer)
-    channelCommentCount = db.Column(db.Integer)
-    channelVideoCount = db.Column(db.Integer)
-    channelKeywords = db.Column(db.ARRAY(db.String))
-    channelTopicIds = db.Column(db.String(10))
+class ChannelList(db.Model):
+    __tablename__ = 'channel_list'
+    channel_id = db.Column(db.String(24), nullable=False, primary_key=True, index=True, unique=True)
 
-    def __init__(self,
-                 channelId, channelTitle,
-                 channelDescription, channelCustomUrl,
-                 channelPublishedAt, channelThumbnailsUrl,
-                 channelCountry, channelRelatedPlaylists,
-                 channelViewCount, channelCommentCount,
-                 channelVideoCount, channelKeywords,
-                 channelTopicIds):
-        self.channelId = channelId
-        self.channelTitle = channelTitle
-        self.channelDescription = channelDescription
-        self.channelCustomUrl = channelCustomUrl
-        self.channelPublishedAt = channelPublishedAt
-        self.channelThumbnailsUrl = channelThumbnailsUrl
-        self.channelCountry = channelCountry
-        self.channelRelatedPlaylists = channelRelatedPlaylists
-        self.channelViewCount = channelViewCount
-        self.channelCommentCount = channelCommentCount
-        self.channelVideoCount = channelVideoCount
-        self.channelKeywords = channelKeywords
-        self.channelTopicIds = channelTopicIds
+    def __init__(self, **kwargs):
+        super(ChannelList, self).__init__(**kwargs)
+
+
+class ChannelSnippet(db.Model):
+    __tablename__ = 'channel_snippet'
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    channel_id = db.Column(db.String(24),
+                           ForeignKey("channel_list.channel_id"),
+                           nullable=False)
+    channel_title = db.Column(db.String(100), nullable=False)
+    channel_description = db.Column(db.String(1000), nullable=False)
+    channel_custom_url = db.Column(db.String(100), nullable=False)
+    channel_published_at = db.Column(db.DateTime, nullable=False)
+    channel_thumbnails_url = db.Column(db.String(200), nullable=False)
+    channel_country = db.Column(db.String(2), nullable=False)
+    update_time = db.Column(db.DateTime, default=func.now(), nullable=False)
+
+    def __init__(self, **kwargs):
+        super(ChannelSnippet, self).__init__(**kwargs)
+
+
+class ChannelStatistics(db.Model):
+    __tablename__ = 'channel_statist'
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    channel_id = db.Column(db.String(24),
+                           ForeignKey("channel_list.channel_id"),
+                           nullable=False)
+    view_count = db.Column(db.BIGINT, nullable=False, default=0)
+    comment_count = db.Column(db.BIGINT, nullable=False, default=0)
+    subscriber_count = db.Column(db.BIGINT, nullable=False, default=0)
+    video_count = db.Column(db.BIGINT, nullable=False, default=0)
+    hidden_subscriber_count = db.Column(db.BOOLEAN, nullable=False)
+    update_time = db.Column(db.DateTime, default=func.now(), nullable=False)
+
+    def __init__(self, **kwargs):
+        super(ChannelStatistics, self).__init__(**kwargs)
+
+
+class ChannelContentDetail(db.Model):
+    __tablename__ = 'channel_content_detail'
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    channel_id = db.Column(db.String(24),
+                           ForeignKey("channel_list.channel_id"),
+                           nullable=False)
+    channel_related_playlists = db.Column(db.String(24), nullable=False)
+    channel_keywords = db.Column(db.JSON)
+    channel_topic_id = db.Column(db.JSON)
+
+    def __init__(self, **kwargs):
+        super(ChannelContentDetail, self).__init__(**kwargs)
 
 
 class TopicIds(db.Model):
-    __tablename__ = 'topic_id'
-    id = db.Column(db.String(10), primary_key=True, nullable=False)
-    topicName = db.Column(db.String, nullable=False)
+    __tablename__ = 'channel_topic'
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    channel_id = db.Column(db.String(24),
+                           ForeignKey("channel_list.channel_id"),
+                           nullable=False)
+    topic_name = db.Column(db.JSON, nullable=False)
 
-    def __init__(self, topicName):
-        self.topicName = topicName
+    def __init__(self, topicName, channelId):
+        self.topic_name = topicName
+        self.channel_id = channelId
 
 
 class Keywords(db.Model):
-    __tablename__ = 'channelKeywords'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    keyword = db.Column(db.String(20), nullable=False)
+    __tablename__ = 'channel_keywords'
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    channel_id = db.Column(db.String(24),
+                           ForeignKey("channel_list.channel_id"),
+                           nullable=False)
+    keyword = db.Column(db.JSON, nullable=False)
 
-    def __init__(self, keyword):
+    def __init__(self, keyword, channelId):
         self.keyword = keyword
+        self.channel_id = channelId
 
 
 class ChannelPlaylistItem(db.Model):
     __tablename__ = 'channel_playlist_items'
-    videoId = db.Column(db.String(11), primary_key=True, nullable=False)
-    videoPublishedAt = db.Column(db.DateTime, nullable=False)
-    channelId = db.Column(db.String(24), nullable=False)
+    video_id = db.Column(db.String(11), primary_key=True, nullable=False)
+    video_published_at = db.Column(db.DateTime, nullable=False)
+    channel_id = db.Column(db.String(24), nullable=False)
 
     def __init__(self, videoId, videoPublishedAt, channelId):
-        self.videoId = videoId
-        self.videoPublishedAt = videoPublishedAt
-        self.channelId = channelId
+        self.video_id = videoId
+        self.video_published_at = videoPublishedAt
+        self.channel_id = channelId
 
 
 class VideoDetail(db.Model):
     __tablename__ = 'video_detail'
-    videoId = db.Column(
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    video_id = db.Column(
         db.String(11),
-        ForeignKey('channel_playlist_items.videoId'),
+        ForeignKey('channel_playlist_items.video_id'),
         nullable=False,
-        primary_key=True
     )
     title = db.Column(db.String(70), nullable=False)
     description = db.Column(db.String(1000), nullable=True)
-    tags = db.Column(db.ARRAY(db.String))
-    categoryId = db.Column(db.Integer)
-    defaultAudioLanguage = db.Column(db.String(5))
-    liveBroadcastContent = db.Column(db.String())
-    viewCount = db.Column(db.Integer)
-    likeCount = db.Column(db.Integer)
-    dislikeCount = db.Column(db.Integer)
-    favoriteCount = db.Column(db.Integer)
-    commentCount = db.Column(db.Integer)
+    tags = db.Column(db.JSON, nullable=False, default=None)
+    category_id = db.Column(db.SMALLINT, nullable=False)
+    default_audio_language = db.Column(db.String(5), nullable=False)
+    live_broadcast_content = db.Column(db.String, nullable=False)
 
-    def __init__(self,
-                 videoId, title,
-                 description, tags,
-                 categoryId, defaultAudioLanguage,
-                 liveBroadcastContent, viewCount,
-                 likeCount, dislikeCount,
-                 favoriteCount, commentCount):
-        self.videoId = videoId
-        self.title = title
-        self.description = description
-        self.tags = tags
-        self.categoryId = categoryId
-        self.defaultAudioLanguage = defaultAudioLanguage
-        self.liveBroadcastContent = liveBroadcastContent
-        self.viewCount = viewCount
-        self.likeCount = likeCount
-        self.dislikeCount = dislikeCount
-        self.favoriteCount = favoriteCount
-        self.commentCount = commentCount
+    def __init__(self, **kwargs):
+        super(VideoDetail, self).__init__(**kwargs)
+
+
+class VideoStatistics(db.Model):
+    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True, nullable=False)
+    video_id = db.Column(
+        db.String(11),
+        ForeignKey('channel_playlist_items.video_id'),
+        nullable=False,
+    )
+    view_count = db.Column(db.BIGINT, nullable=False, default=0)
+    like_count = db.Column(db.BIGINT, nullable=False, default=0)
+    dislike_count = db.Column(db.BIGINT, nullable=False, default=0)
+    favorite_count = db.Column(db.BIGINT, nullable=False, default=0)
+    comment_count = db.Column(db.BIGINT, nullable=False, default=0)
+
+    def __init__(self, **kwargs):
+        super(VideoStatistics, self).__init__(**kwargs)
 
 
 class TopLevelComment(db.Model):
     __tablename__ = 'top_level_comment'
-    CommentId = db.Column(db.String(26), primary_key=True, nullable=False)
-    videoId = db.Column(db.String(11), nullable=False)
-    textOriginal = db.Column(db.String, nullable=False)
-    authorDisplayName = db.Column(db.String, nullable=False)
-    authorChannelId = db.Column(db.String, nullable=False)
-    likeCount = db.Column(db.Integer, nullable=False)
-    publishedAt = db.Column(db.DateTime, nullable=False)
-    updatedAt = db.Column(db.DateTime, nullable=False)
+    comment_id = db.Column(db.String(26), primary_key=True, nullable=False)
+    video_id = db.Column(db.String(11), nullable=False)
+    text_original = db.Column(db.String, nullable=False)
+    author_display_name = db.Column(db.String, nullable=False)
+    author_channel_id = db.Column(db.String, nullable=False)
+    like_count = db.Column(db.Integer, nullable=False, default=0)
+    published_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self,
-                 commentId, videoId,
-                 textOriginal, authorDisplayName,
-                 authorChannelId, likeCount,
-                 publishedAt, updatedAt):
-        self.CommentId = commentId
-        self.videoId = videoId
-        self.textOriginal = textOriginal
-        self.authorDisplayName = authorDisplayName
-        self.authorChannelId = authorChannelId
-        self.likeCount = likeCount
-        self.publishedAt = publishedAt
-        self.updatedAt = updatedAt
+    def __init__(self, **kwargs):
+        super(TopLevelComment, self).__init__(**kwargs)
 
 
 class RepliesComments(db.Model):
     __tablename__ = 'replies_comments'
-    commentsId = db.Column(db.String(49), primary_key=True, nullable=False)
-    parentId = db.Column(db.String(26), nullable=False)
-    videoId = db.Column(db.String(11), nullable=False)
-    textOriginal = db.Column(db.String, nullable=False)
-    authorDisplayName = db.Column(db.String, nullable=False)
-    authorChannelId = db.Column(db.String, nullable=False)
-    likeCount = db.Column(db.Integer, nullable=False)
-    publishedAt = db.Column(db.DateTime, nullable=False)
-    updatedAt = db.Column(db.DateTime, nullable=False)
+    comments_id = db.Column(db.String(49), primary_key=True, nullable=False)
+    parent_id = db.Column(
+        db.String(26),
+        ForeignKey('top_level_comment.comment_id'),
+        nullable=False,
+        primary_key=True
+    )
+    video_id = db.Column(db.String(11), nullable=False)
+    text_original = db.Column(db.String, nullable=False)
+    author_display_name = db.Column(db.String, nullable=False)
+    author_channel_id = db.Column(db.String, nullable=False)
+    like_count = db.Column(db.Integer, nullable=False, default=0)
+    published_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self,
-                 commentId, videoId, parentId,
-                 textOriginal, authorDisplayName,
-                 authorChannelId, likeCount,
-                 publishedAt, updatedAt):
-        self.commentsId = commentId
-        self.parentId = parentId
-        self.videoId = videoId
-        self.textOriginal = textOriginal
-        self.authorDisplayName = authorDisplayName
-        self.authorChannelId = authorChannelId
-        self.likeCount = likeCount
-        self.publishedAt = publishedAt
-        self.updatedAt = updatedAt
+    def __init__(self, **kwargs):
+        super(RepliesComments, self).__init__(**kwargs)
