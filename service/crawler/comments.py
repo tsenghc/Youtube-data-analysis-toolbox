3143c225e5b773cefff05ec5c866a0a6467fcfb7
tsenghc <danny2953053@gmail.com>
2020-09-07 04:01:34 +0800

    Add traverse video comments
    新增遍歷影片留言功能

--- service/crawler/comments.py
+++ service/crawler/comments.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from oauth.api_oauth import youtube
-from utils import error_code
+from service.oauth.api_oauth import youtube
+from service.utils import error_code
 
 
 def get_video_comments(videoId: str, maxResult: int, pageToken=None, order="time") -> dict:
@@ -37,3 +37,49 @@ def get_video_comments(videoId: str, maxResult: int, pageToken=None, order="time
         return response
 
     return error_code.VIDEO_COMMENTS_ERROR
+
+
+def foreach_video_comments(videoId: str) -> dict:
+    """Traverse comments
+
+    Args:
+        videoId: Youtube playlist Id
+
+    Returns:
+        [dict]:Video comments
+        [int]:(3430)PLAYLIST_ITEMS_ERROR
+
+    """
+    top_comments_list = []
+    replies_comments_list = []
+    comments = {}
+    video_comments = get_video_comments(videoId, maxResult=100)
+    if not isinstance(video_comments, dict):
+        return error_code.VIDEO_COMMENTS_ERROR
+
+    if video_comments.get("error", False):
+        return video_comments
+
+    token = video_comments.get("nextPageToken", False)
+
+    if not token:
+        for items in video_comments["items"]:
+            top_comments_list.append(items["snippet"]["topLevelComment"])
+            if "replies" in items:
+                replies_comments_list.append(items["replies"]["comments"])
+        comments['top_comments_list'] = top_comments_list
+        comments['replies_comments_list'] = replies_comments_list
+        return comments
+
+    while token:
+        token = video_comments.get("nextPageToken", False)
+        for items in video_comments["items"]:
+            top_comments_list.append(items["snippet"]["topLevelComment"])
+            if "replies" in items:
+                replies_comments_list.append(items["replies"]["comments"])
+        comments['top_comments_list'] = top_comments_list
+        comments['replies_comments_list'] = replies_comments_list
+        video_comments = get_video_comments(videoId, maxResult=100, pageToken=token)
+
+    if isinstance(top_comments_list, list):
+        return comments
