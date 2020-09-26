c8d15cb4fd38e6ea51189c3290f0babbc60747e5
tsenghc <danny2953053@gmail.com>
2020-09-27 07:48:07 +0800

    Update main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
-from robot.video_bot import most_popular_job
+
 from crawler import videos
 from storage import video_storage
+from robot import channel_bot, video_bot
+from storage import utils
 if __name__ == '__main__':
+    """Please run setup first
 
-    most_popular_job("TW")
+    """
+    # utils.save_video_categories("TW")
+    # video_bot.most_popular_job("TW")
+    # channel_bot.channel_crawler()
+    # channel_bot.channel_detail_crawler()
+    # utils.get_db_channel_list()
+    # print(len(utils.get_playlist_item_id()))
+    # print(len(utils.channel_list_except()))
+    # channel_bot.channel_detail_crawler()
