3f2c5da2cc1e1f82a59711b480ea101d311cb3c7
tsenghc <danny2953053@gmail.com>
2020-09-27 07:42:26 +0800

    Update utils tool
    新增兩張表的頻道ID取得方法，跟一個playlist item表與channel差集的方法

--- src/storage/utils.py
+++ src/storage/utils.py
@@ -1,8 +1,9 @@
 from .flask_app import create_app
 from crawler import videos
-from models.model import ChannelPlaylistItem, VideoDetail, VideoStatistics, db, MostPopular, VideoCategory
-from .playlist_storage import save_channel_playlist_items, save_channel_videoid
+from models.model import ChannelList, ChannelPlaylistItem,   db,  VideoCategory
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import distinct
+
 app = create_app('development')
 
 
@@ -39,3 +40,34 @@ def get_db_video_category(regionCode: str):
     except SQLAlchemyError as e:
         print("check_video_exist:{}".format(type(e)))
     return False
+
+
+def get_db_channel_list():
+    try:
+        with app.app_context():
+            channel_list_id = ChannelList.query.with_entities(
+                ChannelList.channel_id).all()
+            res = [i for (i,) in channel_list_id]
+            return res
+    except SQLAlchemyError as e:
+        print("get_channel_list:{}".format(type(e)))
+    return False
+
+
+def get_playlist_item_id():
+    try:
+        with app.app_context():
+            channel_id = ChannelPlaylistItem.query.with_entities(
+                distinct(ChannelPlaylistItem.channel_id)).all()
+            res = [i for (i,) in channel_id]
+            return res
+    except SQLAlchemyError as e:
+        print("get_playlist_item_id:{}".format(type(e)))
+    return False
+
+
+def channel_list_except():
+    channel_list = get_db_channel_list()
+    playlist_id = get_playlist_item_id()
+    filter_channel = list(set(playlist_id)-set(channel_list))
+    return filter_channel
