92696377d07f22907e55ab6f840d916e61e60011
tsenghc <danny2953053@gmail.com>
2020-09-29 03:55:25 +0800

    Add videoDetail and comment bot
    新增兩隻機器人

--- src/robot/video_bot.py
+++ src/robot/video_bot.py
@@ -1,4 +1,5 @@
-from storage import video_storage, utils
+from storage import video_storage, utils, comment_storage
+import datetime
 
 
 def most_popular_job(region: str):
@@ -6,4 +7,22 @@ def most_popular_job(region: str):
     for i in category_code:
         res = video_storage.save_most_popular_video(
             regionCode=region, videoCategoryId=int(i))
-        print("region:{}|category:{}|{}".format(region, i, res))
+        print("region:{}|category:{}|{}|UpdateTime:{}".format(
+            region, i, res, datetime.datetime.utcnow()))
+
+
+def update_video_detail():
+    video_list = utils.video_detail_except()
+    print(video_list)
+    for i in video_list:
+        if video_storage.save_video_detail(video_id=i):
+            print("Update video:{} detail|UpdateTime:{}".format(
+                i, datetime.datetime.utcnow()))
+
+
+def update_comment():
+    video_list = utils.get_db_ChannelPlayListItem_video_id()
+    for i in video_list:
+        if comment_storage.save_video_comments(video_id=i):
+            print("Update video:{} comment|UpdateTime:{}".format(
+                i, datetime.datetime.utcnow()))
