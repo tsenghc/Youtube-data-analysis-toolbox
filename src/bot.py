b4cc8b4d3d4326342b570486516f745e31bf3353
tsenghc <danny2953053@gmail.com>
2020-09-30 02:04:37 +0800

    Add job
    新增排程工作

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,19 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from robot import video_bot
+from robot import video_bot, channel_bot
 
 scheduler = BlockingScheduler()
+
 scheduler.add_job(video_bot.most_popular_job,
-                  'interval', minutes=30, args=("TW",))
+                  'interval',
+                  minutes=30,
+                  args=("TW",))
+
+scheduler.add_job(video_bot.upload_video_detail,
+                  'interval',
+                  minutes=315)
+
+scheduler.add_job(channel_bot.sync_channelList_channelPlayListItem_channel_id,
+                  'interval',
+                  minutes=50)
+scheduler.add_job(channel_bot.channel_detail_crawler, 'interval', minutes=600)
 scheduler.start()
