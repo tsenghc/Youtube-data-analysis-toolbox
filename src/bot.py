dd266f7e6bc3770da2ed19700dd41fc034350e82
tsenghc <danny2953053@gmail.com>
2020-10-04 16:53:12 +0800

    Update trigger type
    更新觸發器種類為cron

--- src/bot.py
+++ src/bot.py
@@ -1,23 +1,25 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from robot import video_bot, channel_bot
 
-scheduler = BlockingScheduler()
+if __name__ == '__main__':
+    job_defaults = {'max_instances': 2}
+    scheduler = BlockingScheduler(job_defaults=job_defaults)
 
-scheduler.add_job(video_bot.most_popular_job,
-                  'interval',
-                  minutes=30,
-                  args=("TW",))
+    scheduler.add_job(video_bot.most_popular_job,
+                      'cron',
+                      minute=30,
+                      args=("TW",))
 
-scheduler.add_job(video_bot.upload_video_detail,
-                  'interval',
-                  hours=12)
+    scheduler.add_job(video_bot.upload_video_detail,
+                      'cron',
+                      hour=4)
 
-scheduler.add_job(channel_bot.sync_channelList_channelPlayListItem_channel_id,
-                  'interval',
-                  hours=6)
+    scheduler.add_job(channel_bot.sync_channelList_channelPlayListItem_channel_id,
+                      'cron',
+                      hour=6)
 
-scheduler.add_job(channel_bot.channel_detail_crawler_in_region,
-                  'interval',
-                  hours=20,
-                  args=("TW",))
-scheduler.start()
+    scheduler.add_job(channel_bot.channel_detail_crawler_in_region,
+                      'cron',
+                      hour=9,
+                      args=("TW",))
+    scheduler.start()
