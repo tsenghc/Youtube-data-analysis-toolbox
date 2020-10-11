180d51af3bbe3b14ee1b8286bd5d214ada759735
tsenghc <danny2953053@gmail.com>
2020-10-11 13:49:10 +0800

    Update bot schedule

--- src/bot.py
+++ src/bot.py
@@ -4,22 +4,31 @@ from robot import video_bot, channel_bot
 if __name__ == '__main__':
     job_defaults = {'max_instances': 2}
     scheduler = BlockingScheduler(job_defaults=job_defaults)
-
+    start = '2020-10-10 09:30:00'
+    end = '2030-10-10 09:30:00'
     scheduler.add_job(video_bot.most_popular_job,
                       'cron',
-                      minute=30,
-                      args=("TW",))
+                      minute='0/30',
+                      args=("TW",),
+                      start_date=start,
+                      end_date=end)
 
     scheduler.add_job(video_bot.upload_video_detail,
-                      'cron',
-                      hour=4)
+                      'corn',
+                      hour='*/4',
+                      start_date=start,
+                      end_date=end)
 
     scheduler.add_job(channel_bot.sync_channelList_channelPlayListItem_channel_id,
-                      'cron',
-                      hour=6)
+                      'corn',
+                      hour='*/6',
+                      start_date=start,
+                      end_date=end)
 
     scheduler.add_job(channel_bot.channel_detail_crawler_in_region,
-                      'cron',
-                      hour=9,
-                      args=("TW",))
+                      'corn',
+                      hour='*/9',
+                      args=("TW",),
+                      start_date=start,
+                      end_date=end)
     scheduler.start()
