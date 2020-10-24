3144567cfd7025e9c97e54051f806cb38ecc7f16
tsenghc <danny2953053@gmail.com>
2020-10-25 04:04:38 +0800

    Update setup

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
-from storage import utils
+from utils import storage
 if __name__ == '__main__':
     """萬事起於主題
     建議先從熱門影片抓起，爾後取得足夠數量的頻道ID在開始取得頻道訂閱內容與其他相關數據
     """
-    utils.save_video_categories("TW")
+    storage.save_video_categories("TW")
